a080805a6e1ce7673145e29f81cfa8488ac5734e
Wang Liao <wliao7@ucsc.edu>
2023-11-15 10:30:18 -0800

    Import skybox, Add groundCheck/cameraRayCheck

--- Assets/Scripts/First Person Player Controller.cs
+++ Assets/Scripts/First Person Player Controller.cs
@@ -1,9 +1,15 @@
 using System;
+using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class FirstPersonPlayerController : MonoBehaviour
 {
+    [Header("Ray Detection")] 
+    [SerializeField] private Transform groundRayOriginTransform;
+    [SerializeField] private float cameraRayLength;
+    [SerializeField] private float groundRayLength;
+    
     [Header("Player Movement")] 
     [SerializeField] private float moveSpeed;
     [SerializeField] [Range(0, 0.5f)] private float moveLerpFactor;
@@ -16,11 +22,18 @@ public class FirstPersonPlayerController : MonoBehaviour
     [SerializeField] private float cameraHorizontalSpeed;
     [SerializeField] private float cameraVerticalSpeed;
     [SerializeField] [Range(30, 80)] private float angleXMax;
+
+    [Header("Camera Ray UI Text")] 
+    [SerializeField] private TMP_Text cameraRayCheckText;
     
     //Private variables
     private float _xInput;
     private float _yInput;
-    
+    private bool _isGrounded;
+    private LayerMask _onlyGroundLayerMask = 1 << 7;
+    private LayerMask _onlyHandheld = 1 << 8;
+    private LayerMask _ignorePlayerLayerMask = ~(1 << 6);
+
     //Private components
     private Rigidbody _rb;
     
@@ -50,10 +63,13 @@ public class FirstPersonPlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         InitiateMouse();
+        InitiateUI();
     }
 
     private void Update()
     {
+        GroundCheck();
+        CameraRayCheck();
         ReceiveMovementInput();
     }
 
@@ -69,6 +85,66 @@ public class FirstPersonPlayerController : MonoBehaviour
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void InitiateUI()
+    {
+        if (cameraRayCheckText.enabled || cameraRayCheckText.text.Length > 0)
+        {
+            cameraRayCheckText.text = string.Empty;
+            cameraRayCheckText.enabled = false;
+        }
+    }
+    
+    private void GroundCheck()
+    {
+        Ray downRay = new Ray(groundRayOriginTransform.position, Vector3.down);
+        RaycastHit downRayHit;
+        
+        if (Physics.Raycast(downRay, out downRayHit, groundRayLength, _onlyGroundLayerMask))
+        {
+            Debug.DrawRay(downRay.origin, downRay.direction * groundRayLength, Color.green);
+            if (!_isGrounded)
+            {
+                _isGrounded = true;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(downRay.origin, downRay.direction * groundRayLength, Color.red);
+            if (_isGrounded)
+            {
+                _isGrounded = false;
+            }
+        }
+    }
+    
+    private void CameraRayCheck()
+    {
+        Ray cameraRay = new Ray(camTrans.position, camTrans.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(cameraRay, out hit, cameraRayLength, _onlyHandheld))
+        {
+            Debug.DrawRay(cameraRay.origin, cameraRay.direction * cameraRayLength, Color.green);
+
+            if (!cameraRayCheckText.enabled)
+            {
+                cameraRayCheckText.enabled = true;
+            }
+
+            cameraRayCheckText.text = "Interact With Handheld";
+        }
+        else
+        {
+            Debug.DrawRay(cameraRay.origin, cameraRay.direction * cameraRayLength, Color.red);
+            
+            if (cameraRayCheckText.enabled)
+            {
+                cameraRayCheckText.text = String.Empty;
+                cameraRayCheckText.enabled = false;
+            }
+        }
     }
     
     private void ReceiveMovementInput()
@@ -109,38 +185,40 @@ public class FirstPersonPlayerController : MonoBehaviour
 
     private void MouseMovement()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
         
-        Quaternion quaternionX = quaternion.Euler(new Vector3(0, mouseX * cameraHorizontalSpeed * Time.fixedDeltaTime, 0));
-        Quaternion quaternionY = quaternion.Euler(new float3(-mouseY * cameraVerticalSpeed * Time.fixedDeltaTime, 0, 0));
+            Quaternion quaternionX = quaternion.Euler(new Vector3(0, mouseX * cameraHorizontalSpeed * Time.fixedDeltaTime, 0));
+            Quaternion quaternionY = quaternion.Euler(new float3(-mouseY * cameraVerticalSpeed * Time.fixedDeltaTime, 0, 0));
 
-        transform.rotation = quaternionX * transform.rotation;
-        camTrans.rotation = camTrans.rotation * quaternionY;
+            transform.rotation = quaternionX * transform.rotation;
+            camTrans.rotation = camTrans.rotation * quaternionY;
 
-        float angleX = camTrans.localEulerAngles.x;
+            float angleX = camTrans.localEulerAngles.x;
         
-        if (angleX > 180)
-        {
-            angleX -= 360;
-        }
+            if (angleX > 180)
+            {
+                angleX -= 360;
+            }
 
-        if (angleX < -180)
-        {
-            angleX += 360;
-        }
+            if (angleX < -180)
+            {
+                angleX += 360;
+            }
         
-        if (angleX > angleXMax)
-        {
-            //Debug.Log(camTrans.eulerAngles);
-            Debug.Log("Camera rotation on X is higher than max value");
-            camTrans.localEulerAngles = new Vector3(angleXMax, 0, 0);
-        }
+            if (angleX > angleXMax)
+            {
+                //Debug.Log("Camera rotation on X is higher than max value");
+                camTrans.localEulerAngles = new Vector3(angleXMax, 0, 0);
+            }
 
-        if (angleX < -angleXMax)
-        {
-            Debug.Log("Camera rotation on X is lower than min value");
-            camTrans.localEulerAngles = new Vector3(-angleXMax, 0, 0);
+            if (angleX < -angleXMax)
+            {
+                //Debug.Log("Camera rotation on X is lower than min value");
+                camTrans.localEulerAngles = new Vector3(-angleXMax, 0, 0);
+            }
         }
     }
 }
