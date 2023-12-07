76a337c2de8b19d91f136113350ee7381d4d2069
ofcourseitsgv <gavilori@ucsc.edu>
2023-12-06 17:23:44 -0800

    water = death, click to activate handheld

--- Assets/Scripts/2D Game/Wooden Plank Script.cs
+++ Assets/Scripts/2D Game/Wooden Plank Script.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class WoodenPlankScript : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem explosionParticles;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ground") || other.CompareTag("DeadZone"))
         {
+            Instantiate(explosionParticles, gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Water : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            SceneManager.LoadScene("Main Scene");
+        }
+    }
+}
