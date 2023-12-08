68ae0bd14139de99e1e5af81fe7ccd43ec9b512f
ichigooo33 <wliao7@ucsc.edu>
2023-12-08 11:17:05 -0800

    add cross fade animation / add cross fade canvas

--- Assets/LevelGoal.cs
+++ Assets/LevelGoal.cs
@@ -7,10 +7,19 @@ using UnityEngine.SceneManagement;
 public class LevelGoal : MonoBehaviour
 {
     public string nextScene;
+    [SerializeField] private Animator crossFadeAnimator;
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") {
-            SceneManager.LoadScene(nextScene);
+            //SceneManager.LoadScene(nextScene);
+            StartCoroutine(LoadNextScene());
         }
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        crossFadeAnimator.SetTrigger("Cross Fade Start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(nextScene);
     }
 }
