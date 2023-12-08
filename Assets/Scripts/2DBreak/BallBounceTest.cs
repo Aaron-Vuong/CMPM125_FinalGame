95c10fed54066323be8cb6e7ce6925ba00c13ef7
Aaron-Vuong <81384460+Aaron-Vuong@users.noreply.github.com>
2023-12-08 01:07:37 -0800

    Improving breakout ball bounce.

---
+++ Assets/Scripts/2DBreak/BallBounceTest.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BallBounceTest : MonoBehaviour
+{
+    private int speed = 10;
+    private Vector3 direction;
+
+    [SerializeField] private ParticleSystem explosionParticles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.down; //new Vector3(Random.Range(-0.5f, 0.5f),-1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("2DPlayer"))
+        {
+            if (collision.rigidbody != null)
+            {
+                // Reflect in the direction that we are moving and keep the same magnitude.
+                Vector3 new_direction = direction.normalized + collision.rigidbody.velocity.normalized;
+                direction = new_direction.normalized * direction.magnitude;
+            }
+        }
+        // Apply the reflection force to the ball
+        direction = Vector3.Reflect(direction, collision.GetContact(0).normal);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("block"))
+        {
+            Instantiate(explosionParticles, collision.gameObject.transform.position, Quaternion.identity);
+            MiniGamePlayerController.Instance.blocks.Remove(collision.gameObject);
+            Destroy(collision.gameObject, 0.1f);
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            loseLife();
+        }
+    }
+
+    private void loseLife()
+    {
+        Debug.Log("Life Lost");
+        transform.gameObject.SetActive(false);
+    }
+}
